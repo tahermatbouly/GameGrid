0101876fd30780b71b91d51b5e749941ace6a20f
Karim Elbanna <karimelbanna383@gmail.com>
2024-11-24 17:50:58 +0200

    removed pixilated picture

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function switchToLoginPage(){
 
 
 
-let images = ["Images/pixilated-controllers.webp", "Images/pixilated-citycopy.jpg", "Images/wallpaperflare.com_wallpaper.jpg", "Images/wallpaperflare.com_wallpaper5.jpg", "Images/wallpaperflare.com_wallpaper4.jpg", "Images/wallpaperflare.com_wallpaper3.jpg", "Images/wallpaperflare.com_wallpaper2.jpg"]
+let images = ["Images/pixilated-controllers.webp", "Images/wallpaperflare.com_wallpaper.jpg", "Images/wallpaperflare.com_wallpaper5.jpg", "Images/wallpaperflare.com_wallpaper4.jpg", "Images/wallpaperflare.com_wallpaper3.jpg", "Images/wallpaperflare.com_wallpaper2.jpg"]
 let i = 0;
 function slideRight() {
 
@@ -24,16 +24,14 @@ function slideRight() {
     if(i==0){
         document.getElementById('adventures').style.color = 'rgb(0, 255, 234)'
     }else if(i == 1){
-        document.getElementById('adventures').style.color = 'rgb(255, 0, 162)'
-    }else if(i == 2){
         document.getElementById('adventures').style.color = 'rgb(119, 0, 203)'
-    }else if(i == 3){
+    }else if(i == 2){
         document.getElementById('adventures').style.color = 'rgb(0, 145, 255)'
-    }else if(i == 4){
+    }else if(i == 3){
         document.getElementById('adventures').style.color = 'rgb(255, 62, 62)'
-    }else if(i == 5){
+    }else if(i == 4){
         document.getElementById('adventures').style.color = 'rgb(186, 255, 249)'
-    }else if(i == 6){
+    }else if(i == 5){
         document.getElementById('adventures').style.color = 'rgb(255, 129, 38)'
     }
 
