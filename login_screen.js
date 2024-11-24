cccd40e88d486b0c69be94ce3a192b8910ef2d82
taher <taher.mohamed1@msa.edu.eg>
2024-11-25 00:17:47 +0200

    SOME JS CHANGES FOR THE LOGIN FILE

--- login_screen.js
+++ login_screen.js
@@ -1,3 +1,30 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateInputs();
+    checkInputs();
+});
+
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === "") {
+        setErrorFor(email, "Email cannot be blank");    
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === "") {
+        setErrorFor(password, "Password cannot be blank");
+    } else {
+        setSuccessFor(password);
+    }
+};
+
 function switchToWelcomePage(){
     window.location.href = "index.html";
 }
