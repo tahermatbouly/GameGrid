0e2b250a5b8a3d19d26c8f219733f9bf90ae96f3
taher <taher.mohamed1@msa.edu.eg>
2024-11-25 02:40:41 +0200

    some js lines for the signup page

--- login_screen.js
+++ login_screen.js
@@ -8,13 +8,35 @@ form.addEventListener("submit", (e) => {
     checkInputs();
 });
 
+const setErrorFor = (input, message) => {
+    const inputControl = input.parentElement;
+    const errorDisplay = inputControl.querySelector("small");
+    const small = inputControl.querySelector("small");
+    inputControl.className = "form-control error";
+    small.innerText = message;
+    errorDisplay.innerText = message;
+};
+
+const setSuccessFor = (input) => {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+};
+
+
+
 const validateInputs = () => {
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
 
     if (emailValue === "") {
         setErrorFor(email, "Email cannot be blank");    
-    } else {
+    }
+    else if (!isValidEmail(emailValue)) {
+        setErrorFor(email, "Provide a valid email address");
+    } 
+    else {
         setSuccessFor(email);
     }
 
@@ -25,6 +47,3 @@ const validateInputs = () => {
     }
 };
 
-function switchToWelcomePage(){
-    window.location.href = "index.html";
-}
